4d6ab52f6157d982ac3b0eeb70ea4366bb8b5140
bishopkai <123687632+bishopkai@users.noreply.github.com>
2024-05-14 16:12:07 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,28 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD97v0JOhU8Vfdaz4EoxA-OMTErUPS9w2k",
+    authDomain: "project-seven-c31f2.firebaseapp.com",
+    databaseURL: "https://project-seven-c31f2-default-rtdb.firebaseio.com",
+    projectId: "project-seven-c31f2",
+    storageBucket: "project-seven-c31f2.appspot.com",
+    messagingSenderId: "257034431192",
+    appId: "1:257034431192:web:04384bd1e17a619940c5ad",
+    measurementId: "G-3N15F5HGYP"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+
 //login.js content
 document.addEventListener('DOMContentLoaded', function () {
     const loginLink = document.querySelector('.login-link');
